ff10951c9f5b3af634ed5f2c5b9398aabee57be0
young891221 <young891221@gmail.com>
2017-12-13 11:22:08 +0900

    original junit1

---
+++ src/main/java/com/junit/original/framework/AssertionFailedError.java
@@ -0,0 +1,15 @@
+
+package com.junit.original.framework;
+
+/**
+ * Thrown when an assertion failed.
+ */
+public class AssertionFailedError extends Error {
+
+    public AssertionFailedError () {
+    }
+
+    public AssertionFailedError (String message) {
+        super (message);
+    }
+}
